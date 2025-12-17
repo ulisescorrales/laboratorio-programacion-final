2660f31dc6883ed26535c53ffb1475a80f2ffd6b
Ulises Corrales <ulisescorralesallen@gmail.com>
2025-12-17 10:25:08 -0300

    primer componente generado

--- LaBarbeerApp/app/_layout.tsx
+++ LaBarbeerApp/app/_layout.tsx
@@ -18,12 +18,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
