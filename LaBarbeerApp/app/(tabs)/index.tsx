6da863d6e2d8069047135c9afe7663d8b18e54f4
Ulises Corrales <ulisescorralesallen@gmail.com>
2025-12-19 13:29:06 -0300

    19-dic-2025

--- LaBarbeerApp/app/(tabs)/index.tsx
+++ LaBarbeerApp/app/(tabs)/index.tsx
@@ -3,8 +3,54 @@ import { Image } from "react-native";
 import { StyleSheet } from "react-native";
 import Cortes from "../componentes/cortes";
 import Cervezas from "../componentes/cervezas";
-
+import Seccion from "../componentes/seccion"
+import Producto from "../../components/interfaces/Producto"
 export default function HomeScreen() {
+  const colCortes: Producto[] = [
+    {
+      nombre: "Corte Clásico",
+	  descripcion:"Corte tradicional y prolijo, ideal para quienes buscan un look formal y atemporal. Se trabaja principalmente con tijera y terminaciones limpias.",
+      marca: null,
+      precio: 10000,
+      promocion: null,
+      pathImagen: require("../../assets/images/cortes/corte1.jpg")
+    },
+    {
+      nombre: "Corte a tijera",
+	  descripcion: "Corte realizado íntegramente con tijera, logrando un acabado natural, con volumen y movimiento.",
+      marca: null,
+      precio: 12000,
+      promocion: null,
+      pathImagen: require("../../assets/images/cortes/corte2.jpg")
+    },
+    {
+      nombre: "Corte + afeitada",
+	  descripcion:"Todo el cabello al mismo largo, ofreciendo un estilo simple y uniforme.",
+      marca: null,
+      precio: 15000,
+      promocion: null,
+      pathImagen: require("../../assets/images/cortes/corte3.jpg")
+    },
+    {
+      nombre: "Estilo urbano",
+	  descripcion:"Corte sencillo y práctico, fácil de mantener, ideal para niños y adolescentes.",
+      marca: null,
+      precio: 11000,
+      promocion: null,
+      pathImagen: require("../../assets/images/cortes/corte4.jpg")
+    },
+  ];
+  const cortes = [
+    require("../../assets/images/cervezas/kuruf1.jpg"),
+    require("../../assets/images/cervezas/kuruf2.jpg"),
+    require("../../assets/images/cervezas/kuruf3.jpg"),
+    require("../../assets/images/cervezas/kuruf4.jpg"),
+    require("../../assets/images/cervezas/kuruf5.jpg"),
+    require("../../assets/images/cervezas/kuruf6.jpg"),
+    require("../../assets/images/cervezas/kuruf7.jpg")]
+
+  
+
   return (
     <ImageBackground source={require("../../assets/images/fondobarberia2.jpg")} style={{height:"100%"}} >
       <View>
@@ -13,7 +59,7 @@ export default function HomeScreen() {
           style={styles.titleContainer}
         />
         <ScrollView>
-          <Cortes />
+		  <Seccion title={"Nuestros Cortes"} colProductos={colCortes} />
 		  <Cervezas />
         </ScrollView>
       </View>
