5d03fcd7c3540391b38c9a92f7d8192199788bb2
Ulises Corrales <ulisescorralesallen@gmail.com>
2025-12-19 13:29:06 -0300

    19-dic-2025

--- LaBarbeerApp/app/componentes/cortes.tsx
+++ LaBarbeerApp/app/componentes/cortes.tsx
@@ -1,4 +1,5 @@
-import { Text, View, FlatList } from "react-native";
+import { StyleSheet } from 'react-native'
+import { Text,GestureResponderEvent, Pressable, View, FlatList } from "react-native";
 import { Image } from "react-native";
 import { styles } from "../(tabs)/index";
 import Producto from "@/components/interfaces/Producto";
@@ -7,33 +8,40 @@ export default function Cortes() {
   const colCortes: Producto[] = [
     {
       nombre: "Corte Clásico",
+	  descripcion:"Corte tradicional y prolijo, ideal para quienes buscan un look formal y atemporal. Se trabaja principalmente con tijera y terminaciones limpias.",
       marca: null,
       precio: 10000,
       promocion: null,
-      pathImagen: "../../assets/images/cortes/corte1.jpg",
+      pathImagen: require("../../assets/images/cortes/corte1.jpg")
     },
     {
-      nombre: "Corte facha",
+      nombre: "Corte a tijera",
+	  descripcion: "Corte realizado íntegramente con tijera, logrando un acabado natural, con volumen y movimiento.",
       marca: null,
       precio: 12000,
       promocion: null,
-      pathImagen: "../../assets/images/cortes/corte2.jpg",
+      pathImagen: require("../../assets/images/cortes/corte2.jpg")
     },
     {
       nombre: "Corte + afeitada",
+	  descripcion:"Todo el cabello al mismo largo, ofreciendo un estilo simple y uniforme.",
       marca: null,
       precio: 15000,
       promocion: null,
-      pathImagen: "../../assets/images/cortes/corte3.jpg",
+      pathImagen: require("../../assets/images/cortes/corte3.jpg")
     },
     {
       nombre: "Estilo urbano",
+	  descripcion:"Corte sencillo y práctico, fácil de mantener, ideal para niños y adolescentes.",
       marca: null,
       precio: 11000,
       promocion: null,
-      pathImagen: "../../assets/images/cortes/corte4.jpg",
+      pathImagen: require("../../assets/images/cortes/corte4.jpg")
     },
   ];
+  // const cortes = colCortes.map((corte) => corte.pathImagen);
+
+  // const cortesRef = useRef(null);
   //
   // const cortes = [
   //   require("../../assets/images/cortes/corte1.jpg"),
@@ -51,21 +59,43 @@ export default function Cortes() {
         key='flatListCortes'
         numColumns={2}
         scrollEnabled={false}
-        data={cortes}
+        data={colCortes}
         renderItem={({ item, index }) => (
           <View style={{ margin: 10 }}>
-            <Image
-              source={item}
-              key={index}
-              style={[styles.image]}
-              borderRadius={16}
-            />
+            <Pressable
+			onPress={(event:GestureResponderEvent)=>{
+			  // event.currentTarget.
+			}}
+			>
+              <Image
+                source={item.pathImagen}
+                key={index}
+                style={[styles.image]}
+                borderRadius={16}
+              />
+			  //Info del corte
+			  <View style={styleItem.itemCard}>
+				<Text style={styleItem.textItem}>{item.nombre}</Text>
+				<Text style={styleItem.textItem}>{item.precio}$</Text>
+			  </View>
+            </Pressable>
           </View>
         )}
       />
     </View>
   );
 }
+const styleItem=StyleSheet.create({
+  textItem:{
+	textAlign:'center',
+	color:'black',
+	fontSize:16
+  },
+  itemCard:{
+	backgroundColor:'white',
+    borderRadius:15
+  }
+})
 // <View style={styles.containerCortes}>
 //   {cortes.map((imgPath, index) => (
 
---
+++ LaBarbeerApp/app/componentes/seccion.tsx
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native'
+import { Text,GestureResponderEvent, Pressable, View, FlatList } from "react-native";
+import { Image } from "react-native";
+import { styles } from "../(tabs)/index";
+import Producto from "@/components/interfaces/Producto";
+
+type seccionProps={
+  title:string,
+  colProductos:Producto[];
+}
+
+export default function Seccion({title,colProductos}:seccionProps){
+
+  return (
+    <View style={styles.containerCenter}>
+      <View style={styles.containerTitle}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      <FlatList
+        key='flatListCortes'
+        numColumns={2}
+        scrollEnabled={false}
+        data={colProductos}
+        renderItem={({ item, index }) => 
+		  (<View style={{ margin: 10 }}>
+            <Pressable >
+              <Image
+                source={item.pathImagen}
+                key={index}
+                style={[styles.image]}
+                borderRadius={16}
+              />
+			  <View style={styleItem.itemCard}>
+				<Text style={styleItem.textItem}>{item.nombre}</Text>
+				<Text style={styleItem.textItem}>{item.precio}$</Text>
+			  </View>
+            </Pressable>
+          </View>
+        )}/>
+    </View>
+  );
+}
+const styleItem=StyleSheet.create({
+  textItem:{
+	textAlign:'center',
+	color:'black',
+	fontSize:16
+  },
+  itemCard:{
+	backgroundColor:'white',
+    borderRadius:15
+  }
+})
