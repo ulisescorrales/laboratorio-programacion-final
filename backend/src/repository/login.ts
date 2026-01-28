0a5cfe066deb7655d6b9cb6ae456cca496328afb
ulisescorrales <ulisescorralesallen@gmail.com>
2026-01-28 19:45:19 -0300

    refactor: repartidas las responsabilidades entre controller-service-repository en login

--- backend/src/repository/login.ts
+++ backend/src/repository/login.ts
@@ -1,1 +1,8 @@
 export const users=new Map()
+export const insertarUser=(user:string,hash:string)=>{
+	users.set(user,hash)
+}
+
+export const getHash=(user:string)=>{
+	return users.get(user)
+}
--- backend/src/service/login.ts
+++ backend/src/service/login.ts
@@ -1,8 +1,61 @@
-import { users } from "../repository/login"
+import * as loginRepository from "../repository/login"
+const jwt=require('jsonwebtoken');
+const secreto='secreto'
 const bcrypt=require('bcrypt')
+const saltRounds=10;
 
 export const compararContrasenias=async (user:string,password:string)=>{
-	 await bcrypt.compare(password,users.get(user),(err:any,result:any)=>{
+	return new Promise((reject,resolv)=>{
+		try {
+			const hash=loginRepository.getHash(user)
+			bcrypt.compare(password,hash,(err:any,result:any)=>{
+			if(err){
+				reject(err)
+			}else{
+				if(result){
+					resolv("Contraseñas coinciden")
+				}else{
+					reject("Password Incorrecta")
+				}
+			}
+		})
+		}catch{
+			reject("Error en BD")
+		}
+	})
+}
+export const generarUsuario=(user:string,password:string)=>{
+	return new Promise((resolv,reject)=>{
+		bcrypt.genSalt(saltRounds,(err:any,salt:any)=>{
+			if(err){
+				reject(err)
+			}
+			bcrypt.hash(password,salt,(err:any,hash:any)=>{
+				if(err){
+					reject(err)
+				}else{
+					loginRepository.insertarUser(user,hash)
+					resolv("Usuario creado")
+				}
+			})
+		})
 
+	})
+}
+export const generarToken=(user:string,role:string)=>{
+	return 	jwt.sign({
+				user: user,
+				role:'normal'
+			},secreto,{expiresIn:'1h'})
+}
+export const getRole=(token:string)=>{
+	return new Promise<string>((resolv,reject)=>{
+		jwt.verify(token,secreto,(err:any,decoded:any)=>{
+			if(err){
+				reject(err)
+			}else{
+				resolv(decoded)
+			}
+		})
 	})
 }
