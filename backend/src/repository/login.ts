c54238f275be7ba700c158b253388a2d4b1a0445
ulisescorrales <ulisescorralesallen@gmail.com>
2026-02-01 17:05:54 -0300

    feat: generado requests de login y autenticación para con la BD

--- backend/src/repository/login.ts
+++ backend/src/repository/login.ts
@@ -1,8 +1,36 @@
-export const users=new Map()
-export const insertarUser=(user:string,hash:string)=>{
-	users.set(user,hash)
-}
+import { pool } from '../config';
+export const users = new Map();
+export const insertarUser = (user: string, hash: string) => {
+	// users.set(user,hash)
+	return new Promise((resolv, reject) => {
+		pool.query(
+			'INSERT INTO usuario VALUES (?,?)',
+			[user, hash],
+			(err, result, fields) => {
+				if (err) {
+					console.log(err);
+					reject('Error insertando usuario');
+				} else {
+					resolv(result);
+				}
+			}
+		);
+	});
+};
 
-export const getHash=(user:string)=>{
-	return users.get(user)
-}
+export const getHash = (user: string) => {
+	return new Promise((resolv, reject) => {
+		pool.query(
+			'SELECT `hash` FROM `usuario` WHERE `nombre_usuario` = ? ;',
+			[user],
+			(err, result: any, fields) => {
+				if (err) {
+					console.log(err);
+					reject('Error en la consulta del hash del usuario');
+				} else {
+					resolv(result[0].hash);
+				}
+			}
+		);
+	});
+};
