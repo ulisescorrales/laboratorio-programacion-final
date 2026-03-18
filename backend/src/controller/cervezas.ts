5160fc0039ad0ecf8220481fab47e64e0bf658e8
Ulises Corrales <ulisescorralesallen@gmail.com>
2026-03-18 12:26:25 -0300

    feat: replicado el backend de cortes sobre cervezas

--- backend/src/controller/cervezas.ts
+++ backend/src/controller/cervezas.ts
@@ -1,10 +1,78 @@
 import * as cervezasService from '../service/cervezas'
 export const getCervezas=async (req:any,res:any)=>{
 	try{
-		const cervezas=await cervezasService.getCervezasJSON()
+		const cervezas=await cervezasService.getCervezasJSON();
 		res.status(200).json(cervezas);
-	}catch (err){
+	}catch(err){
 		res.status(500).send("Error obteniendo cervezas")
 	}
-};
+}
+export const getCerveza=async(req:any,res:any)=>{
+	const nombre=req.params.nombre
+	try{
+		const cerveza=await cervezasService.getCervezaJSON(nombre);
+		res.status(200).json(cerveza)
+	}catch(err:any){
+		//err contiene el código de error
+		const status=Number(err.message)
+		let mensaje;
+		if(status==404){
+			mensaje="No existe el cerveza especificado"
+		}else{
+			mensaje="Error consultando cerveza"
+		}
+		res.status(status).send(mensaje)
+	}
+}
 
+export const registrarCerveza=async(req:any,res:any)=>{
+	const nombre=req.body.nombre;
+	const descripcion=req.body.descripcion;
+	const marca = req.body.marca
+	let precio= req.body.precio
+	const imagen = req.file;
+	if(precio){
+		precio=Number(precio)
+	}else{
+		res.send(400).send("Falta el precio")
+	}
+	if( nombre && descripcion && marca && precio && imagen ){
+		//TODO: guardar la imagen y obtener el path relativo
+		try{
+				await cervezasService.registrarCervezaService(nombre,descripcion,marca,precio,imagen)
+				res.status(200).send("Guardado con éxito")
+		}catch(err){
+				res.status(500).send("No se pudo guardar en la BD")
+		}
+	}else{
+		res.status(400).send("Faltan datos en el body")
+	}
+}
+export const borrarCerveza=async(req:any,res:any)=>{
+	const nombre=req.params.nombre
+	try{
+		const exito=await cervezasService.borrarCerveza(nombre);
+		if(exito){
+			res.status(200).send("Elemento borrado correctamente")
+		}else{
+			res.status(404).send("Elemento no existe")
+		}
+	}catch(err){
+		res.status(500).send("Error del servidor al intentar borrar el elemento")
+	}
+}
+export const modificarCerveza=async(req:any,res:any)=>{
+	const nombre=req.params.nombre
+	const body=req.body
+	const imagen = req.file;
+	try{
+		const exito= await cervezasService.modificarCervezaService(nombre,body,imagen)
+		if(exito){
+			res.status(200).send("OK")
+		}else{
+			res.status(404).send("Corte no encontrado")
+		}
+	}catch(err){
+		res.status(500).send("Error en el server")
+	}
+}
--- backend/src/service/cervezas.ts
+++ backend/src/service/cervezas.ts
@@ -1,9 +1,60 @@
 import * as cervezasRepository from '../repository/cervezas'
 
-export const getCervezasJSON=async()=>{
+export const getCervezasJSON=async ()=>{
 	try{
-		return await cervezasRepository.getCervezasBD()
+		const cervezas=await cervezasRepository.getCervezasBD()
+		return cervezas
 	}catch(err:any){
-	 	throw new Error(err)
+		throw new Error(err.message)
 	}
+}
+export const getCervezaJSON=async(nombre:string)=>{
+	try{
+		const corte=await cervezasRepository.getCervezaBD(nombre)
+		return corte;
+	}catch(err:any){
+		throw new Error(err)
+	}
+}
+export const registrarCervezaService=async(nombre:string,descripcion:string,marca:string,precio:number,imagen:any)=>{
+	let pathImagen:string|null=null;
+	try{
+		pathImagen=await cervezasRepository.guardarImagenEnFS(imagen);
+		await cervezasRepository.insertarCervezaBD(nombre,descripcion,marca,precio,pathImagen)
+	}catch(err:any){
+		if(pathImagen){
+			//Borrar imagen
+			cervezasRepository.borrarImagenCervezaPorPath(pathImagen)
+		}
+		throw new Error(err.message)
+	}
+}
+export const borrarCerveza=async(nombre:string)=>{
+	try{
+	let exito=await cervezasRepository.borrarImagenCervezaPorId(nombre)
+	if(exito){
+		exito=await cervezasRepository.borrarCervezaBD(nombre);
+	}
+	return exito
+	}catch(err:any){
+		throw new Error(err.message)
+	}
+}
+export const  modificarCervezaService=async (id:string,body:any,imagen:any)=>{
+	//si no se cargó una imagen desde el frontend, ignorar
+	try{
+		const exito= await cervezasRepository.modificarCervezaBD(id,body)
+		let pathImagen;
+		if(imagen){
+			await cervezasRepository.borrarImagenCervezaPorId(id)
+			pathImagen=await cervezasRepository.guardarImagenEnFS(imagen)
+			await cervezasRepository.guardarImagenEnBD(id,pathImagen)
+		}
+		return exito;
+	}catch(err){
+		throw new Error("Error en cervezas service");
+	}
+}
+export const borrarImagenCerveza=async (pathImagen:string)=>{
+	await cervezasRepository.borrarImagenCervezaPorPath(pathImagen)
 }
